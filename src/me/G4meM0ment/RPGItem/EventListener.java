514933c2a6ad093dd441fcca93b6f79c31ede0b5
J <g4mem0ments@gmx.net>
2013-06-25 14:37:56 +0200

    Updated for CB 1.5.2-R1.1; fixed various NPEs

--- src/me/G4meM0ment/RPGItem/EventListener.java
+++ src/me/G4meM0ment/RPGItem/EventListener.java
@@ -1,73 +1,86 @@
 package me.G4meM0ment.RPGItem;
 
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class EventListener implements Listener {
 	
-	RPGItem plugin;
-	public EventListener(RPGItem RPGItem)
-	{
-	    Bukkit.getServer().getPluginManager().registerEvents(this, RPGItem);
-	    plugin = RPGItem;
+	private RPGItem plugin;
+	
+	public EventListener(RPGItem plugin) {
+	    this.plugin = plugin;
 	}
 	
 	//Listeners to repair on Events which damages the items
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-	public void onEntityDamage(EntityDamageEvent event)
-	{
+	public void onEntityDamage(EntityDamageEvent event) {
 		//checks if its a player
-		if((event.getEntity() instanceof Player))
-		{
-			Player damaged = (Player) event.getEntity();
+		if((event.getEntity() instanceof Player)) {
+			Player p = (Player) event.getEntity();
 			//sends the player which inventory should be repaired to the repair method
-			plugin.repair(damaged);
+			plugin.repair(p);
 		} 
 	}
 	
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
+	public void onBlockDamage(BlockDamageEvent event) {
+		Player p = (Player) event.getPlayer();
+		plugin.repair(p);
+	}
+	
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-	public void onPlayerDeath(PlayerDeathEvent event)
-	{
-		Player killed = (Player) event.getEntity();
-		plugin.repair(killed);
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		Player p = (Player) event.getEntity();
+		plugin.repair(p);
 	}
 	
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-	public void onInventoryOpen(InventoryOpenEvent event)
-	{
-		Player opener = (Player) event.getPlayer();
-		plugin.repair(opener);
+	public void onInventoryOpen(InventoryOpenEvent event) {
+		Player p = (Player) event.getPlayer();
+		plugin.repair(p);
 	}
 	
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-	public void onInventoryClose(InventoryCloseEvent event)
-	{
-		Player closer = (Player) event.getPlayer();
-		plugin.repair(closer);
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL) 
+	public void onInventoryClose(InventoryCloseEvent event) {
+		Player p = (Player) event.getPlayer();
+		plugin.repair(p);
 	}
 
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-	public void onPlayerJoin(PlayerJoinEvent event)
-	{
-		Player joiner = event.getPlayer();
-		plugin.repair(joiner);
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player p = event.getPlayer();
+		plugin.repair(p);
 	}
-	
+
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-	public void onPlayerInteract(PlayerInteractEvent event)
-	{
-		Player interacter = event.getPlayer();
-		plugin.repair(interacter);
+	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+		if(event.getEntity() instanceof Player) {
+			Player player = (Player) event.getEntity();
+			ItemStack[] armor = player.getInventory().getArmorContents();
+			int health = player.getHealth();
+			
+			ItemStack helmet = armor[0];
+			ItemStack chest = armor[1];
+			ItemStack leg = armor[2];
+			ItemStack boot = armor[3];
+			if(helmet != null && helmet.getType() != Material.AIR) {
+				Material material = helmet.getType();
+				//if()
+			}
+		}
+		
 	}
-	
 	//TODO add support for plugins which damages items, add more listeners which repairs
 }
--- src/me/G4meM0ment/RPGItem/RPGItem.java
+++ src/me/G4meM0ment/RPGItem/RPGItem.java
@@ -1,182 +1,218 @@
 package me.G4meM0ment.RPGItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RPGItem extends JavaPlugin {
 	
-	RPGItem rpgi;
+	private EventListener eventlistener;
 	
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		PluginDescriptionFile pdf = getDescription();
-		new EventListener(this);
-		rpgi = this;
+
+		eventlistener = new EventListener(this);
+		getServer().getPluginManager().registerEvents(eventlistener, this);
 		
 		//Config creation
-		this.getConfig().options().copyDefaults(true);
-		this.saveConfig();
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 
 		//Initialize messages on server start
-		this.getLogger().info("Version: " + pdf.getVersion());
-		if(this.getConfig().getBoolean("permissions") == true)
-		{
-			this.getLogger().info("Permissions are enabled!");
-		}
-		if(this.getConfig().getBoolean("permissions") == false)
-		{
-			this.getLogger().info("Permissions are disabled!");
-		}
-		if(this.getConfig().getBoolean("items") == true)
-		{
-			this.getLogger().info("Items are enabled!");
-		}
-		if(this.getConfig().getBoolean("items") == false)
-		{
-			this.getLogger().info("Items are disabled!");
-		}
-		if(this.getConfig().getBoolean("armor") == true)
-		{
-			this.getLogger().info("Armor is enabled!");
-		}
-		if(this.getConfig().getBoolean("armor") == false)
-		{
-			this.getLogger().info("Armor is disabled!");
-		}
-		this.getLogger().info("RPGItem enabled v" + pdf.getVersion() + " (by G4meM0ment)!");
+		getLogger().info("Version: " + pdf.getVersion());
+		if(getConfig().getBoolean("permissions"))
+			getLogger().info("Permissions are enabled!");
+		
+		if(!getConfig().getBoolean("permissions"))
+			getLogger().info("Permissions are disabled!");
+		
+		if(getConfig().getBoolean("items"))
+			getLogger().info("Items are enabled!");
+		
+		if(!getConfig().getBoolean("items"))
+			getLogger().info("Items are disabled!");
+		
+		if(getConfig().getBoolean("armor"))
+			getLogger().info("Armor is enabled!");
+		
+		if(!getConfig().getBoolean("armor"))
+			getLogger().info("Armor is disabled!");
+		
+		getLogger().info("RPGItem enabled v" + pdf.getVersion() + " (by G4meM0ment)!");
 	}
 	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-	{
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player player = (Player) sender;
 		
-		if(command.getName().equalsIgnoreCase("rpgitem") && args.length > 0 && args[0].equals("reload") && (player.hasPermission("rpgitem.reload") || player.hasPermission("rpgitem.admin") || player.isOp()))
-		{
-			this.reloadConfig();
-			this.saveConfig();
+		if(command.getName().equalsIgnoreCase("rpgitem") && args.length > 0 && args[0].equals("reload") && checkReloadPerms(player) && sender instanceof Player) {
+			reloadConfig();
+			saveConfig();
 			player.sendMessage("RPGItem reloaded!");
-			this.getLogger().info("Reloaded config!");
+			getLogger().info("Reloaded config!");
 		    return true;
+		}
+		if(command.getName().equalsIgnoreCase("rpgitem") && args.length > 0 && args[0].equals("reload") && checkReloadPerms(player) && !(sender instanceof Player)) {
+			reloadConfig();
+			saveConfig();
+			getLogger().info("Reloaded config!");
 		}
 		return false;
 	}
 	
-	public void repair(Player player)
+	public void repair(Player p)
 	{
-		if(player != null)
-		{
+		if(p != null) {
+			PlayerInventory inv = p.getInventory();
+			ItemStack[] arm = inv.getArmorContents();
+			List<String> sItems = getConfig().getStringList("repair_list");
+			List<Material> materials = new ArrayList<Material>();
 			short repaired;
-			//Checks if permissions are enabled
-			if(this.getConfig().getBoolean("permissions") == true && (player.hasPermission("rpgitem.repairs") || player.hasPermission("rpgitem.admin") || player.isOp()))
-			{
+			
+			if(getConfig().getBoolean("use_default_list"))
+				materials = defaultList();
+			else {
+				for(String sItem : sItems) {
+					materials.add(getMaterial(sItem));
+				}
+			}
+
+			//Sets the repair value
+			if(getConfig().getBoolean("default_durability_bar"))
+				repaired = 1;
+			else
+				repaired = 0;
+			
+			//Check for permissions
+			if(checkRepairPerms(p)) {
 				//Checks if items are enabled
-				if(this.getConfig().getBoolean("items") == true)
-				{
-					//checks if the durability bar is default
-					if(this.getConfig().getBoolean("default_durability_bar") == false)
-					{
-						repaired = 0;
-						for(ItemStack item : player.getInventory())
-						{
-							if(item != null && item.getType() != Material.AIR)
+				if(getConfig().getBoolean("items")) {
+					for(ItemStack item : inv) {
+						//checks if the item was damaged and it is an item
+						if(item != null && item.getType() != Material.AIR && materials.contains(item.getType()))
+							try{
 								item.setDurability(repaired);
-						}
-					}
-					//activates if the durability bar isn't enabled
-					else
-					{
-						repaired = 1;
-						for(ItemStack item : player.getInventory())
-						{
-							if(item != null && item.getType() != Material.AIR)
-								item.setDurability(repaired);
-						}
+							}catch(NullPointerException e) {
+								
+							}
 					}
 				}
 				//checks if armor is enabled
-				if(this.getConfig().getBoolean("armor") == true)
-				{
-					if(this.getConfig().getBoolean("default_durability_bar") == false)
-					{
-						repaired = 0;
-						for(ItemStack item : player.getInventory().getArmorContents())
-						{
-							if(item != null && item.getType() != Material.AIR)
-								item.setDurability(repaired);
-						}
-					}			
-					else
-					{
-						repaired = 1;
-						for(ItemStack item : player.getInventory().getArmorContents())
-						{
-							if(item != null && item.getType() != Material.AIR)
-								item.setDurability(repaired);
-						}
+				if(!getConfig().getBoolean("armor")) {
+					for(ItemStack item : arm) {
+						if(item != null && item.getType() != Material.AIR && materials.contains(item.getType()))
+							item.setDurability(repaired);
 					}
-				}
-			}
-			else
-			{
-				if(this.getConfig().getBoolean("items") == true)
-				{
-			
-					if(this.getConfig().getBoolean("default_durability_bar") == false)
-					{
-						repaired = 0;
-						for(ItemStack item : player.getInventory())
-						{
-							if(item != null && item.getType() != Material.AIR)
-								item.setDurability(repaired);
-						}
-					}
-			
-					else
-					{
-						repaired = 1;
-						for(ItemStack item : player.getInventory())
-						{
-							if(item != null && item.getType() != Material.AIR)
-								item.setDurability(repaired);
-						}
-					}
-				}
-				if(this.getConfig().getBoolean("armor") == true)
-				{
-					if(this.getConfig().getBoolean("default_durability_bar") == false)
-					{
-						repaired = 0;
-						for(ItemStack item : player.getInventory().getArmorContents())
-						{
-							if(item != null && item.getType() != Material.AIR)
-								item.setDurability(repaired);
-						}
-					}
-			
-					else
-					{
-						repaired = 1;
-						for(ItemStack item : player.getInventory().getArmorContents())
-						{
-							if(item != null && item.getType() != Material.AIR)
-								item.setDurability(repaired);
-						}
-					}
-				}
+				}			
 			}
 		}
 	}
 	
+	private boolean checkRepairPerms(Player p) {
+		if(getConfig().getBoolean("permissions")) {
+			if((p.hasPermission("rpgitem.repairs") || p.hasPermission("rpgitem.admin")))
+				return true;
+			else
+				return false;				
+		}
+		else
+			return true;
+	}
+	
+	private boolean checkReloadPerms(Player p) {
+		if(getConfig().getBoolean("permissions")) {
+			if((p.hasPermission("rpgitem.reload") || p.hasPermission("rpgitem.admin")))
+				return true;
+			else
+				return false;				
+		}
+		else
+			if(p.isOp())
+				return true;
+			else
+				return false;
+	}
+	
+	//gets the id or the name of the item from config and returns it as Material
+	private Material getMaterial(String id)
+	{
+		Material get = Material.getMaterial(id);
+		if(get != null) return get;
+	try
+	{
+		get = Material.getMaterial(Integer.valueOf(id));
+	}
+	catch(NumberFormatException e)
+	{
+	}
+	   return get;
+	}
+	
+	private List<Material> defaultList() {
+		List<Material> list = new ArrayList<Material>();
+		list.add(Material.BOW);
+		list.add(Material.CHAINMAIL_BOOTS);
+		list.add(Material.CHAINMAIL_CHESTPLATE);
+		list.add(Material.CHAINMAIL_HELMET);
+		list.add(Material.CHAINMAIL_LEGGINGS);
+		list.add(Material.DIAMOND_AXE);
+		list.add(Material.DIAMOND_BOOTS);
+		list.add(Material.DIAMOND_CHESTPLATE);
+		list.add(Material.DIAMOND_HELMET);
+		list.add(Material.DIAMOND_LEGGINGS);
+		list.add(Material.DIAMOND_HOE);
+		list.add(Material.DIAMOND_PICKAXE);
+		list.add(Material.DIAMOND_SPADE);
+		list.add(Material.DIAMOND_SWORD);
+		list.add(Material.FISHING_ROD);
+		list.add(Material.FLINT_AND_STEEL);
+		list.add(Material.GOLD_AXE);
+		list.add(Material.GOLD_BOOTS);
+		list.add(Material.GOLD_CHESTPLATE);
+		list.add(Material.GOLD_HELMET);
+		list.add(Material.GOLD_HOE);
+		list.add(Material.GOLD_LEGGINGS);
+		list.add(Material.GOLD_SPADE);
+		list.add(Material.GOLD_SWORD);
+		list.add(Material.IRON_AXE);
+		list.add(Material.IRON_BOOTS);
+		list.add(Material.IRON_CHESTPLATE);
+		list.add(Material.IRON_HELMET);
+		list.add(Material.IRON_HOE);
+		list.add(Material.IRON_LEGGINGS);
+		list.add(Material.IRON_PICKAXE);
+		list.add(Material.IRON_SPADE);
+		list.add(Material.IRON_SWORD);
+		list.add(Material.LEATHER_BOOTS);
+		list.add(Material.LEATHER_CHESTPLATE);
+		list.add(Material.LEATHER_HELMET);
+		list.add(Material.LEATHER_LEGGINGS);
+		list.add(Material.SHEARS);
+		list.add(Material.STONE_AXE);
+		list.add(Material.STONE_HOE);
+		list.add(Material.STONE_PICKAXE);
+		list.add(Material.STONE_SPADE);
+		list.add(Material.STONE_SWORD);
+		list.add(Material.WOOD_HOE);
+		list.add(Material.WOOD_PICKAXE);
+		list.add(Material.WOOD_SPADE);
+		list.add(Material.WOOD_AXE);
+		list.add(Material.WOOD_SWORD);
+		return list;
+	} 
+	
 	@Override
 	public void onDisable()
 	{		
-		this.getLogger().info("RPGItem disabled!");
+		getLogger().info("RPGItem disabled!");
 	}
 
 }
